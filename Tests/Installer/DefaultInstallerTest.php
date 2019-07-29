ab3bf790cc2691e6aed1ab85f6cdbfce2658d82d
Hugo Soltys <hugo.soltys@m6.fr>
2019-07-29 17:15:46 +0200

    Adding installer tests

--- Tests/Installer/DefaultInstallerTest.php
+++ Tests/Installer/DefaultInstallerTest.php
@@ -2,6 +2,8 @@
 
 namespace HugoSoltys\FitzBundle\Tests\Installer;
 
+use HugoSoltys\FitzBundle\Helper\FileHelper;
+use HugoSoltys\FitzBundle\Model\AvailableBundles;
 use PHPUnit\Framework\TestCase;
 use HugoSoltys\FitzBundle\Installer\DefaultInstaller;
 
@@ -24,5 +26,26 @@ class DefaultInstallerTest extends TestCase
         $installer->setBundleName('MyBundle');
 
         $this->assertFalse($installer->isInstalled());
+    }
+
+    public function testIsQueued()
+    {
+        $queueFilePath = \sprintf('%s/../../', __DIR__);
+        FileHelper::append(\sprintf('%s/%s', $queueFilePath, AvailableBundles::QUEUE_FILE), 'MyBundle;');
+
+        $installer = new DefaultInstaller('/usr/local/bin/composer', [], '/path/to/my/project', \sprintf('%s/../../', __DIR__));
+        $installer->setBundleName('MyBundle');
+
+        $this->assertTrue($installer->isQueued());
+
+        FileHelper::remove(\sprintf('%s/%s', $queueFilePath, AvailableBundles::QUEUE_FILE), 'MyBundle;');
+    }
+
+    public function testIsNotQueued()
+    {
+        $installer = new DefaultInstaller('/usr/local/bin/composer', [], '/path/to/my/project', \sprintf('%s/../../', __DIR__));
+        $installer->setBundleName('MyBundle');
+
+        $this->assertFalse($installer->isQueued());
     }
 }
